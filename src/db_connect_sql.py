6cdd43c08540daf6ee557f2251da7eec6f68926b
v.zubriichuk <v@zubr.kiev.ua>
2021-03-26 17:58:08 +0200

    Init commit

---
+++ src/db_connect_sql.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Author : Vitaliy Zubriichuk
+Contact : v@zubr.kiev.ua
+Time    : 25.03.2021 16:27
+"""
+import sqlalchemy as sql
+
+server = 'S-KV-CENTER-S64'
+driver = 'SQL+Server'
+db = 'PlanRC'
+engine = sql.create_engine('mssql+pyodbc://{}/{}?driver={}'.format(server, db, driver))
+
+def get_revenues():
+    query = '''
+
+        with Lagers as (
+        select 
+                lagerId
+            ,	lc.lagerSapClassifierId
+            ,	lc.commodityGroupId
+            ,	lg.commodityGroupName
+        from MasterData.sku.lagers l with (nolock)
+            join MasterData.[dbo].[LinkCommodityGroupLagerSapClassifier] lc (nolock)
+                on l.lagerClassifier =  lc.lagerSapClassifierId
+            join MasterData.dbo.ListAssortmentCommodityGroups lg (nolock)
+                on lc.commodityGroupId=lg.commodityGroupId
+        where l.lagerQuality = 0
+            and lc.disableRow = 0
+            and lg.businessId = 1                 --Сильпо
+            and lg.commodityGroupId in (5550499, 5550053, 5550017, 5550022, 
+            5550150, 5550120, 5550151, 5550118, 5550015, 5550050, 5550077)
+        )
+
+        select	
+                filid
+            ,	createddate
+            ,	sum_revenues	=	sum(priceout*kolvo)
+            ,	sum_kolvo		=	sum(kolvo)
+        from Cheques.dbo.view_ChequeLines cl with (nolock)
+            join Lagers l
+                on l.LagerId = cl.LagerID
+        where created between '20210308' and  '20210312'--cast(getdate()-1 as date) 
+            and filid in (	
+                2382, 2129, 2236, 2251, 2085, 2056, 2045, 2025, 1934,
+                1995, 2042, 2145, 2183, 2173, 2197, 2009, 2132, 2071, 2051, 2275, 2121,
+                2261, 2144, 2192, 2107, 2123, 2105, 2087, 2273, 2247, 2226, 2149, 2232, 2052, 1998, 2256, 2187, 2031, 2122, 2268, 2015,
+                2093, 2039, 2043, 2117, 2161, 2029, 2053, 2006, 2127, 2000, 2177, 2010, 2044, 2254, 2086, 2237, 2002, 2290, 2088, 2069,
+                2218, 2092, 1997, 2028, 2023, 2146, 2231, 2106, 2216, 2250, 2128, 2154, 1935, 2022, 2266, 2036, 2260, 2116, 2124, 2001,
+                2035, 1993, 2159, 2220, 1994, 2046, 2223, 2245, 2017, 2114, 2257, 2018, 2032, 2227, 2230, 2055, 2120, 2057, 2189, 2277,
+                2238, 1996, 2047, 2252, 2267, 2246, 2011, 1990, 2240, 2125, 2160, 2004, 2118, 2112, 2013, 2184, 2049, 2038, 2005, 2064,
+                2073, 2012, 1999, 2225, 2034, 2221, 2090, 2115, 2233, 2195, 2048, 2014, 2062, 2262, 2040, 2060, 2037, 2191, 2126, 2016,
+                2253, 2234, 2176, 2186, 2054, 2156, 2185, 2258, 2259, 2041, 2026, 1992, 2130, 2059, 2133, 2196, 2222, 2242, 2224, 2131,
+                2153, 2151, 2027, 2019, 2008, 2113, 2219, 2077, 2078, 2188, 2108, 2134, 1991, 2021, 2143, 2020, 2279, 2070, 2270, 2119,
+                2291, 2239, 2063, 2024, 2281, 2269, 2265, 2217, 2091, 2007, 2198, 2244, 2241, 2248, 2050, 2190, 2058, 2214, 2061, 2030,
+                2201, 2157, 2170, 2194, 2171, 2158, 2147, 2213, 2148, 2155, 2152, 2199, 2215, 2276, 2212, 2274, 2243, 2150, 2280, 2264,
+                2072, 2033, 2229
+            )
+        group by FilID, CreatedDate
+
+           '''
+    return query
+
---
+++ src/msp_dashboard.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Author : Vitaliy Zubriichuk
+Contact : v@zubr.kiev.ua
+Time    : 25.03.2021 16:32
+"""
+from scipy.stats import mannwhitneyu, wilcoxon
+from datetime import date, datetime
+import db_connect_sql as conn
+import numpy as np
+import pandas as pd
+import xlrd
+
+# Список филиалов на которых проводился пилот
+pg_filials = [1934, 2022, 2031, 2069, 2112, 2120, 2131, 2254, 2382, 2028, 1999]
+commodity_groups = [5550499, 5550053, 5550017, 5550022, 5550150, 5550120,
+                    5550151, 5550118, 5550050, 5550077]
+
+# Выгрузка данных по чекам за период
+# sales = pd.read_sql_query(conn.get_revenues(), conn.engine)
+sales = pd.read_csv('sales_5550499_commodity_group.csv')
+
+data_for_all_filials = sales.copy(deep=True)
+data_for_all_filials['createddate'] = pd.to_datetime(
+    data_for_all_filials['createddate'])
+
+start_pilot = pd.to_datetime('2021-03-08')
+pilot = data_for_all_filials[data_for_all_filials['createddate'] >= start_pilot]
+
+end_pilot = pilot.createddate.max()
+time_series_pilot = pilot.groupby(['filid', 'createddate']).sum()
+
+# print(time_series_pilot.loc[1934]['sum_revenues'])
+
+filials = np.unique(data_for_all_filials.filid)
+candidate_for_cg_filials = filials[~np.isin(filials, pg_filials)]
+
+time_series_pivoted = pd.DataFrame(columns=['dates_pilot', 'time_series_pilot'])
+
+for filid in filials:
+    y = time_series_pilot.loc[filid]['sum_revenues']
+    y = pd.DataFrame(pd.date_range(start=start_pilot, end=end_pilot),
+                     columns=['createddate']).merge(y.reset_index(),
+                                                    how='left').set_index(
+        'createddate')['sum_revenues'].fillna(0)
+
+    time_series_pivoted.loc[filid, 'time_series_pilot'] = np.array(y)
+    time_series_pivoted.loc[filid, 'dates_pilot'] = np.array(y.index)
+
+# Подготавливаем датафреймы
+result_tg_fil = pd.DataFrame(
+    columns=['commodityGroupId', 'Filid', 'EffectValue', 'DecisionValue',
+             'modifiedDate'])
+result_tg = pd.DataFrame(
+    columns=['commodityGroupId', 'EffectValue', 'DecisionValue',
+             'modifiedDate'])
+result_total = pd.DataFrame(
+    columns=['EffectValue', 'DecisionValue', 'modifiedDate'])
+
+# Получение весов
+weights = pd.read_excel('weights_shops_msp.xlsx').set_index('index')
+
+# Эффект на уровне ТГ-Филиал
+for tg in commodity_groups:
+    weights_commodity_group = weights[weights.commodity_group == tg].fillna(
+        0).to_dict()
+
+    # Подсчет выручек для синтетических контрольных магазинов
+    pg_daily_revenues = []
+    cg_daily_revenues = []
+
+    for pg_filial in pg_filials:
+        w = weights_commodity_group[pg_filial]
+        values = []
+        for k, v in w.items():
+            values.append(
+                time_series_pivoted['time_series_pilot'].to_dict()[k] * v)
+
+        cg_pilot = np.vstack(values).sum(axis=0)
+        pg_pilot = time_series_pivoted['time_series_pilot'].to_dict()[pg_filial]
+
+        pg_daily_revenues.append(pg_pilot)
+        cg_daily_revenues.append(cg_pilot)
+
+        # Подсчет эффекта и статистического решения
+        effect = (pg_pilot.sum() / cg_pilot.sum()) - 1
+        _, p_value = mannwhitneyu(cg_pilot, pg_pilot,
+                                  use_continuity=False, alternative='greater')
+
+        df = pd.DataFrame([[tg, pg_filial, effect, p_value, date.today()]],
+                          columns=['commodityGroupId', 'Filid', 'EffectValue',
+                                   'DecisionValue', 'modifiedDate'])
+        result_tg_fil = result_tg_fil.append(df, ignore_index=True,
+                                             verify_integrity=False,
+                                             sort=None)
+
+print(result_tg_fil)
+# print(df.iloc[:1])
+
+# Цикл эффекта на уровне ТГ
+for tg in commodity_groups:
+    weights_commodity_group = weights[weights.commodity_group == tg].fillna(
+        0).to_dict()
+
+    # Подсчет выручек для синтетических контрольных магазинов
+    pg_daily_revenues = []
+    cg_daily_revenues = []
+
+    for pg_filial in pg_filials:
+        w = weights_commodity_group[pg_filial]
+        values = []
+        for k, v in w.items():
+            values.append(
+                time_series_pivoted['time_series_pilot'].to_dict()[k] * v)
+
+        cg_pilot = np.vstack(values).sum(axis=0)
+        pg_pilot = time_series_pivoted['time_series_pilot'].to_dict()[pg_filial]
+
+        pg_daily_revenues.append(pg_pilot)
+        cg_daily_revenues.append(cg_pilot)
+
+    pg_daily_revenues = np.array(pg_daily_revenues).flatten()
+    cg_daily_revenues = np.array(cg_daily_revenues).flatten()
+
+    # Подсчет эффекта и статистического решения
+    effect = (pg_daily_revenues.sum() / cg_daily_revenues.sum()) - 1
+    _, p_value = mannwhitneyu(cg_daily_revenues, pg_daily_revenues,
+                              use_continuity=False, alternative='greater')
+
+    df = pd.DataFrame([[tg, effect, p_value, date.today()]],
+                      columns=['commodityGroupId', 'EffectValue',
+                               'DecisionValue', 'modifiedDate'])
+    result_tg = result_tg.append(df, ignore_index=True, verify_integrity=False,
+                                 sort=None)
+
+print(result_tg)
+
+# Эффект на уровне всех ТГ
+pg_total_revenues = []
+cg_total_revenues = []
+
+for tg in commodity_groups:
+    weights_commodity_group = weights[weights.commodity_group == tg].fillna(
+        0).to_dict()
+
+    # Подсчет выручек для синтетических контрольных магазинов
+
+    pg_daily_revenues = []
+    cg_daily_revenues = []
+
+    for pg_filial in pg_filials:
+        w = weights_commodity_group[pg_filial]
+        values = []
+        for k, v in w.items():
+            values.append(
+                time_series_pivoted['time_series_pilot'].to_dict()[k] * v)
+
+        cg_pilot = np.vstack(values).sum(axis=0)
+        pg_pilot = time_series_pivoted['time_series_pilot'].to_dict()[pg_filial]
+
+        pg_daily_revenues.append(pg_pilot)
+        cg_daily_revenues.append(cg_pilot)
+
+    pg_total_revenues.append(pg_daily_revenues)
+    cg_total_revenues.append(cg_daily_revenues)
+
+pg_total_revenues = np.array(pg_total_revenues).flatten()
+cg_total_revenues = np.array(cg_total_revenues).flatten()
+
+# Подсчет эффекта и статистического решения
+effect = (pg_total_revenues.sum() / cg_total_revenues.sum()) - 1
+_, p_value = mannwhitneyu(cg_total_revenues, pg_total_revenues,
+                          use_continuity=False, alternative='greater')
+
+result_total = pd.DataFrame([[effect, p_value, date.today()]],
+                            columns=['EffectValue', 'DecisionValue',
+                                     'modifiedDate'])
+
+print(result_total)
